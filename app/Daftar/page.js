407eba6c924f280f340080815d3031fd2852e60b
audialrsaa <araspxr@gmail.com>
2025-11-11 03:41:41 +0700

    dlt

--- app/Daftar/page.js
+++ app/Daftar/page.js
@@ -1,8 +1,5 @@
-import DetailAcaraPian from "@/components/TrackingDetailAcara/TrackingDetailAcara";
 import Footer from "@/components/landing/Footer";
-import TrackingAcara from "@/components/Navbar/TrackingAcara";
 import DaftarPage from "@/components/DaftarPage/daftar";
-
 export default function Masuk() {
     return (
         <>
