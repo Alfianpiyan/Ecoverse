e16f5961487d357a500a1cc1046b4d7a0ba03210
Alfianpiyan <alfianalfatizi@gmail.com>
2025-11-14 21:53:51 +0700

    coment

--- app/donatur/pengaturan/page.js
+++ app/donatur/pengaturan/page.js
@@ -1,7 +1,5 @@
-import DetailAcaraPian from "@/components/TrackingDetailAcara/TrackingDetailAcara";
+
 import Footer from "@/components/landing/Footer";
-import TrackingAcara from "@/components/Navbar/TrackingAcara";
-import MasukPage from "@/components/MasukPage/masuk";
 import NavbarDonatur from "@/components/HomePage/NavbarDonatur";
 import PengaturanAkun from "@/components/pengaturanAkun/PengaturanAkun";
 
